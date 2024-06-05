d56274925f53a577763545b1ca9b1e0da077ec69
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-06-05 18:34:09 +0800

    Add basic functions
    Created functions for addition, subtraction, multiplication, and division, as well as variables to hold the first and second number, and the operator

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstNumber = 0
+let secondNumber = 0
+let operator = ""
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
