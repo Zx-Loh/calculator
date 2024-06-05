101a1d61e244eeefc6906a5dca176cdd312ca9da
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-06-05 18:40:33 +0800

    Add operate function
    operate() takes in 2 numbers as well as a operator, and performs the corresponding mathematical operation on both numbers

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+function operate(a, b, sign) {
+    switch(sign) {
+        case "+":
+            add(a, b)
+            break;
+
+        case "-":
+            subtract(a, b)
+            break;
+
+        case "*":
+            multiply(a, b)
+            break;
+
+        case "/":
+            divide(a, b)
+            break;
+    }
+}
