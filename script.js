bc84b0b4119a09d8a245bb2ed401c718bbce8ad8
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-06-17 18:49:39 +0800

    Add logic for storing user input numbers and operator, and reset function that sets all variables to initial state
    Also added comments explaining some functions, and linked all the code together so calculator can actually perform operations now

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 let firstNumber = ""
 let secondNumber = ""
 let operator = ""
+let ans = 0
 const screen = document.querySelector("#screen")
 const buttons = document.querySelectorAll(".button")
+
+
+function reset() {
+    return firstNumber = "", secondNumber = "", operator = "", ans = 0
+}
 
 
 buttons.forEach((item) => {
     item.addEventListener("click", buttonPress)
 })
 
-function buttonPress(event) {
+//Checks which button was pressed, if CLEAR or DELETE was pressed, calls corresponding functions
+function buttonPress(event) { 
     const buttonText = event.target.innerText
 
     if (buttonText == "CLEAR" && screen.hasChildNodes()) {
         screen.replaceChildren()
+        reset()
 
     } else if (buttonText == "DELETE" && screen.hasChildNodes()) {
         screen.removeChild(screen.lastChild)
 
+    //If = is pressed and user has input 2 numbers + operator, calls operate function to calculate the expression
     } else if (buttonText == "=" && secondNumber && operator) {
-        operate(firstNumber, secondNumber, operator)
+        operate(parseInt(firstNumber), parseInt(secondNumber), operator)
 
+    //If 2 numbers and operatore have not been input by user, calls updateDisplay function
     } else if (buttonText != "DELETE" && buttonText != "CLEAR" && buttonText != "=") {
         updateDisplay(buttonText)
     }
 }
 
+//Checks if there is an existing value for first/second number and operator
+//Updates variable and display accordingly
 function updateDisplay(keyPressed) {
     switch(keyPressed) {
         case "+":
@@ -34,21 +46,26 @@ function updateDisplay(keyPressed) {
         case "*":
             let content = document.createTextNode(keyPressed);
             screen.appendChild(content)
-            firstNumber = keyPressed
             operator = keyPressed
             break
 
         default:
-            if (firstNumber == "") {
-                const buttonText = parseInt(numberPressed)
-                let content = document.createTextNode(buttonText);
+            if (firstNumber == "" && operator == "") {
+                let content = document.createTextNode(keyPressed);
                 screen.appendChild(content)
-                firstNumber = keyPressed
-            } 
-    }
+                firstNumber += keyPressed
 
+            } else {
+                let content = document.createTextNode(keyPressed);
+                screen.appendChild(content)
+                secondNumber += keyPressed
+            }
+    }
+    return operator, firstNumber, secondNumber
 }
 
+
+//Operate
 function add(a, b) {
     return a + b
 }
@@ -68,19 +85,24 @@ function divide(a, b) {
 function operate(a, b, sign) {
     switch(sign) {
         case "+":
-            add(a, b)
+            ans = add(a, b)
             break;
 
         case "-":
-            subtract(a, b)
+            ans = subtract(a, b)
             break;
 
         case "*":
-            multiply(a, b)
+            ans = multiply(a, b)
             break;
 
         case "/":
-            divide(a, b)
+            ans = divide(a, b)
             break;
     }
+
+    screen.replaceChildren()
+    let content = document.createTextNode(ans);
+    screen.appendChild(content)
+    reset()
 }
